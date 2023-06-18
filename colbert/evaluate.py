684fcb02521be6aaeae14a46c47381053c6dcf01
Cheon Jaewon <navistwon97@gmail.com>
2023-06-18 10:35:25 +0000

    FIX: DEBUGGING

--- colbert/evaluate.py
+++ colbert/evaluate.py
@@ -45,9 +45,7 @@ def main():
 
     query = list(val_dataset["query"])
     ground_truth = list(val_dataset["context"])
-
-    batched_p_embs_name = "batched_p_embs.pickle"
-    batched_p_embs_path = os.path.join('/opt/ml/colbert/rank', batched_p_embs_name)
+    
     with torch.no_grad():
         model.eval()
         
@@ -55,59 +53,25 @@ def main():
         q_emb = model.query(**q_seqs_val).to("cpu")
         print('q_emb_size: \n', q_emb.size())
         
-        if os.path.isfile(batched_p_embs_path):
-            with open(batched_p_embs_path, "rb") as file:
-                batched_p_embs = pickle.load(file)
-            print('batched_p_embs_loaded')
+        print("Start passage embedding.. ....")
+        batched_p_embs = []
+        P_BATCH_SIZE = 128
+        # Define a generator for iterating in chunks
+        def chunks(iterable, n, fillvalue=None):
+            args = [iter(iterable)] * n
+            return zip_longest(*args, fillvalue=fillvalue)
+
+        for step, batch in enumerate(tqdm(chunks(context, P_BATCH_SIZE), total=len(context)//P_BATCH_SIZE)):
+            # The last batch can contain `None` values if the length of `context` is not divisible by 128
+            batch = [b for b in batch if b is not None]
+
+            # Tokenize the entire batch at once
+            p = tokenize_colbert(batch, tokenizer, corpus="doc").to("cuda")
+            p_emb = model.doc(**p).to("cpu").numpy()
+            batched_p_embs.append(p_emb)
         
-        else:
-            # batched_p_embs = []
-            # with torch.no_grad():
-
-            #     model.eval()
-
-            #     # 토크나이저
-            #     q_seqs_val = tokenize_colbert(query, tokenizer, corpus="query").to("cuda")
-            #     q_emb = model.query(**q_seqs_val).to("cpu")
-                
-            #     print(q_emb.size())
-                
-            #     print("Start passage embedding......")
-            #     p_embs = []
-            #     for step, p in enumerate(tqdm(context)):
-            #         p = tokenize_colbert(p, tokenizer, corpus="doc").to("cuda")
-            #         p_emb = model.doc(**p).to("cpu").numpy()
-            #         p_embs.append(p_emb)
-            #         if (step + 1) % 200 == 0:
-            #             batched_p_embs.append(p_embs)
-            #             p_embs = []
-            #     batched_p_embs.append(p_embs)
-            
-            # print("passage tokenizing done!!!!")
-            # length = len(val_dataset["context"])
-
-            print("Start passage embedding.. ....")
-            batched_p_embs = []
-            P_BATCH_SIZE = 128
-            # Define a generator for iterating in chunks
-            def chunks(iterable, n, fillvalue=None):
-                args = [iter(iterable)] * n
-                return zip_longest(*args, fillvalue=fillvalue)
-
-            for step, batch in enumerate(tqdm(chunks(context, P_BATCH_SIZE), total=len(context)//P_BATCH_SIZE)):
-                # The last batch can contain `None` values if the length of `context` is not divisible by 128
-                batch = [b for b in batch if b is not None]
-
-                # Tokenize the entire batch at once
-                p = tokenize_colbert(batch, tokenizer, corpus="doc").to("cuda")
-                p_emb = model.doc(**p).to("cpu").numpy()
-                batched_p_embs.append(p_emb)
-            
-            print('p_embs_n_batches: ', len(batched_p_embs))
-            print('in_batch_size:\n', batched_p_embs[0].shape)
-            with open(batched_p_embs_path, 'wb') as f:
-                pickle.dump(batched_p_embs, f, pickle.HIGHEST_PROTOCOL)
-            print("passage embedding saved")
+        print('p_embs_n_batches: ', len(batched_p_embs))
+        print('in_batch_size:\n', batched_p_embs[0].shape)
 
     print("passage tokenizing done!!!!")
     length = len(val_dataset["context"])
@@ -117,8 +81,6 @@ def main():
     print(dot_prod_scores.size())
 
     rank = torch.argsort(dot_prod_scores, dim=1, descending=True).squeeze()
-    # print(dot_prod_scores)
-    # print(rank)
     print(rank.size())
     torch.save(rank, f"/opt/ml/colbert/rank/rank_epoch{epoch}.pth")
 
@@ -126,9 +88,6 @@ def main():
     score = 0
 
     for idx in range(length):
-        # print(dot_prod_scores[idx])
-        # print(rank[idx])
-        # print()
         for i in range(k):
             if ground_truth[idx] == context[rank[idx][i]]:
                 score += 1
